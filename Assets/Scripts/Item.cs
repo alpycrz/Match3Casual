ac1fac138aad73894ebe0f5f78d430797ee48e9b
Alpay <alpaykucuk08@gmail.com>
2024-06-06 07:30:08 +0300

    base mechanics are added.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+[CreateAssetMenu(menuName = "Match3/Item")]
+public class Item : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int value;
+    public Sprite sprite;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class ItemDatabase
+{
+    public static Item[] Items { get; private set; }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize() => Items = Resources.LoadAll<Item>("Items/");
+
+}
